9e06fb55f927e710c4fa3c94f9bac5539c5e9a3b
Justin Williams <justinw@me.com>
2023-05-27 13:02:04 -0600

    Add AEW feed

---
+++ src/algos/aew.ts
@@ -0,0 +1,52 @@
+import { InvalidRequestError } from '@atproto/xrpc-server'
+import { QueryParams } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
+import { AppContext } from '../config'
+
+export const uri =
+  'at://did:plc:ovd4yosoobsdxwmay46wzhwx/app.bsky.feed.generator/aew'
+
+const AEW_ACCOUNTS = [
+  'did:plc:dsmuyt6h5emct7b42qkum5dv', // aew.bsky.social
+  'did:plc:kdugmwhfecul5277pxqwdxsa', // rjcity.bsky.social
+  'did:plc:e2u3jd45hqntyueuucb42g74', // tonykhan.bsky.social
+  'did:plc:khwlu6usc7lc2qzhdbvactg3', // willwashington.bsky.social
+  'did:plc:odbt6gx5okqfvdqcwf72htbl', // iamjericho.bsky.social
+]
+
+export const handler = async (ctx: AppContext, params: QueryParams) => {
+  let builder = ctx.db
+    .selectFrom('post')
+    .selectAll()
+    .where('author', 'in', AEW_ACCOUNTS)
+    .orderBy('indexedAt', 'desc')
+    .orderBy('cid', 'desc')
+    .limit(params.limit)
+
+  if (params.cursor) {
+    const [indexedAt, cid] = params.cursor.split('::')
+    if (!indexedAt || !cid) {
+      throw new InvalidRequestError('malformed cursor')
+    }
+    const timeStr = new Date(parseInt(indexedAt, 10)).toISOString()
+    builder = builder
+      .where('post.indexedAt', '<', timeStr)
+      .orWhere((qb) => qb.where('post.indexedAt', '=', timeStr))
+      .where('post.cid', '<', cid)
+  }
+  const res = await builder.execute()
+
+  const feed = res.map((row) => ({
+    post: row.uri,
+  }))
+
+  let cursor: string | undefined
+  const last = res.at(-1)
+  if (last) {
+    cursor = `${new Date(last.indexedAt).getTime()}::${last.cid}`
+  }
+
+  return {
+    cursor,
+    feed,
+  }
+}
--- src/algos/index.ts
+++ src/algos/index.ts
@@ -4,11 +4,13 @@ import {
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
 import * as mustSee from './must-see'
+import * as aew from './aew'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 
 const algos: Record<string, AlgoHandler> = {
   [mustSee.uri]: mustSee.handler,
+  [aew.uri]: aew.handler,
 }
 
 export default algos
